cb609f673d78aeb887203eca9991f390fa86f654
ngnhatmih <ngnhatmih9997@gmail.com>
2024-01-19 18:05:24 +0700

    Added FileUtils for writing/reading xml files

---
+++ src/main/java/com/swing_ams/ams/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.swing_ams.ams;
+
+/**
+ *
+ * @author ngnhatmih
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
