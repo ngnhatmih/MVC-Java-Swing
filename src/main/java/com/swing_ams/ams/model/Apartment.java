a4bfc4173a858729fa0ab0d6b41ba08bd76ac1d0
ngnhatmih <ngnhatmih9997@gmail.com>
2024-03-23 00:50:44 +0700

    Added more fields to Apartment: - paid: paid or unpaid - owner: owner of apartment's name - date: paid date by one third of year/ year ...

--- src/main/java/com/swing_ams/ams/model/Apartment.java
+++ src/main/java/com/swing_ams/ams/model/Apartment.java
@@ -1,35 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.swing_ams.ams.model;
 import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-/**
- *
- * @author ngnhatmih
- */
+
 @XmlRootElement(name = "apartment")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Apartment implements Serializable {
     private long id;
     private String block;
     private int floor;
+    private String owner;
+
     private ApartmentService service;
     
     public Apartment() {
         id = 0;
+        owner = "";
         block = "";
         floor = 1;
         service = new ApartmentService();
     }
     
-    public Apartment(int id, int floor, String block, ApartmentService service) {
+    public Apartment(int id, String owner,  int floor, String block, ApartmentService service) {
         super();
         this.id = id;
+        this.owner = owner;
         this.floor = floor;
         this.block = block;
         this.service = service;
@@ -59,7 +56,7 @@ public class Apartment implements Serializable {
     public void setBlock(String block) {
         this.block = block;
     }
-
+    
     public ApartmentService getService() {
         return service;
     }
@@ -67,4 +64,14 @@ public class Apartment implements Serializable {
     public void setService(ApartmentService service) {
         this.service = service;
     }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    
+    
 }
--- src/main/java/com/swing_ams/ams/model/ApartmentService.java
+++ src/main/java/com/swing_ams/ams/model/ApartmentService.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.swing_ams.ams.model;
 import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- *
- * @author ngnhatmih
- */
 
 @XmlRootElement(name = "service")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -21,13 +12,51 @@ public class ApartmentService implements Serializable {
     private int electricity;
     private int water;
     private int elevator;
+    private boolean paid;
+    private PaidDate date;
     
-    public ApartmentService() {
-        management = electricity = water = elevator = 0;
+    public static class PaidDate {
+        private OneThird oneThird;
+        private int year;
+        
+        public PaidDate() {
+            oneThird = OneThird.NONE;
+            year = 0;
+        }
+        
+        public PaidDate(OneThird oneThird, int year) {
+            this.oneThird = oneThird;
+            this.year = year;
+        }
+        
+        public OneThird getOneThird() {
+            return oneThird;
+        }
+
+        public void setOneThird(OneThird oneThird) {
+            this.oneThird = oneThird;
+        }
+        
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
     }
     
-    public ApartmentService(int management, int electricity, int water, int elevator) {
+    public ApartmentService() {
+        paid = false;
+        management = electricity = water = elevator = 0;
+        date = new PaidDate();
+        
+    }
+    
+    public ApartmentService(boolean paid, PaidDate data, int management, int electricity, int water, int elevator) {
         super();
+        this.paid = paid;
+        this.date = date;
         this.management = management;
         this.electricity = electricity;
         this.water = water;
@@ -69,5 +98,25 @@ public class ApartmentService implements Serializable {
     public int getTotal()
     {
         return management + electricity + elevator + water;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public PaidDate getDate() {
+        return date;
+    }
+
+    public void setDate(PaidDate date) {
+        this.date = date;
+    }
+       
+    private enum OneThird {
+        NONE, I, II, III, IV
     }
 }
